ee45f68dc433f6cdb5f8267efb3eb811222f437c
Francis Mel <fmeldefo@usc.edu>
2020-01-12 10:44:23 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,25 +1,35 @@
-console.log("Up and running!");
-
-
-
 
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[1];
+function checkForMatch(){
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped "+cardsInPlay[0]);
-
-if(cardsInPlay.length === 2){
-	
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+	if(cardsInPlay[0] == cardsInPlay[1]){
+		console.log("You found a match!");
 	}
 	else{
-		alert("Sorry try again");
+		console.log("Sorry, try again.")
 	}
+	}
+
+
+
+
+function flipCard(cardId){
+
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	}
+	
+	
 }
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
